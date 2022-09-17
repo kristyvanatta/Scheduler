5f29f56a156946f6a9955e76cad9eba0e9d3c208
Kristy Vanatta <kristylvanatta@gmail.com>
2022-09-17 19:05:24 -0400

    updated timeblocks with save buttons and working text areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-var dateDisplayEl = $('#currentDay')
-
-
+var dateDisplayEl = $('#currentDay');
+var currentTime = moment().format('H');
+    console.log(currentTime);
 function displayDate() {
     var rightNow = moment().format('dddd, MMMM Do YYYY');
     dateDisplayEl.text(rightNow);
 }
 
 displayDate();
+
+//use a simular function from above to get current time to work for the past present future stuff possible querysellector for the itme
+
+//write a save to local storage button
+//funcrtion within a function for the input box and the save to local storage button
