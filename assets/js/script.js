833d8c4816c963aa69213be981e2e9182499c63b
Kristy Vanatta <kristylvanatta@gmail.com>
2022-09-17 20:48:55 -0400

    completed the README file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,41 @@
 var dateDisplayEl = $('#currentDay');
 var currentTime = moment().format('H');
-    console.log(currentTime);
+var saveButtonEl = $('.saveBtn');
+var past = past < currentTime;
+var present = currentTime;
+var future = future > currentTime;
+//var textInput = document.querySelector(".text");
+
+    
 function displayDate() {
     var rightNow = moment().format('dddd, MMMM Do YYYY');
     dateDisplayEl.text(rightNow);
 }
 
 displayDate();
+
+
+var now = currentTime;
+console.log(now);
+function backgroundColor(){
+    if (now === currentTime){
+        document.getElementsByClassName(present);
+    }
+    else if (now < currentTime){
+        document.getElementsByClassName(past);
+    }
+    else {
+        document.getElementsByClassName(future);
+    }
+}
+saveButtonEl.on('click', function(event) {
+    event.preventDefault();
+
+    var text = document.querySelector(".text").value;
+    localStorage.setItem("text", text);
+    console.log(value);
+})
+
 
 //use a simular function from above to get current time to work for the past present future stuff possible querysellector for the itme
 
